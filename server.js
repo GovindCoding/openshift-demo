c5edf418d100c8869ef0d8d4a9c20e8f7b5aed77
Govind Ekshinge <govind.ekshinge@anka.co.in>
2021-06-03 11:16:08 +0530

    Github first commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to openshift demo  application." });
 });
 
 require("./app/routes/user.routes.js")(app);
